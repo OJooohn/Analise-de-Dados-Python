7101adb6be096062cc207cad681c42601a497488
John Chappell <john.claude.cameron@gmail.com>
2024-10-23 15:22:26 -0300

    Graficos atualizados

--- main.py
+++ main.py
@@ -10,49 +10,58 @@ print(f'{path}\\vgsales.csv')
 
 df = pd.read_csv(f'{path}\\vgsales.csv')
 # df.head()
+
+# TOTAL DE CAMPOS VAZIOS POR COLUNA
+plt.figure(figsize=(10, 6))
+sns.barplot(y=df.isnull().sum().index, x=df.isnull().sum().values, orient='h')
+
+plt.xlabel('Quantidade')
+plt.ylabel('Dados não preenchidos por coluna')
+plt.title('Campo')
+
+plt.show()
+
 df['Year'] = df['Year'].fillna(df['Year'].median())
-df.isnull().sum()
-
 df['Publisher'] = df['Publisher'].fillna(df['Publisher'].mode()[0])
-df.isnull().sum()
 
-# Total de Vendas por Plataforma
+# TOTAL DE VENDAS POR PLATAFORMA
 sales_per_platform = df.groupby('Platform')['Global_Sales'].sum() / 1000
 
 sales_per_platform = sales_per_platform.reset_index()
 order = sales_per_platform.sort_values(by='Global_Sales', ascending=False)['Platform']
 
 plt.figure(figsize=(15, 6))
-sns.barplot(x='Platform', y='Global_Sales', data=sales_per_platform, order=order)
+sns.barplot(x='Platform', y='Global_Sales', data=sales_per_platform, order=order, color='#dc6fed')
 
 plt.title('Total de Vendas por Plataforma')
 plt.ylabel('Total de Vendas (em bilhão)')
 
+plt.grid(True, linestyle='--', alpha=0.7, color='gray')
+
 plt.xticks(rotation=90)
 plt.show()
 
-# Vendas Totais por regiao
+# TOTAL DE VENDAS POR REGIÃO
 regional_sales = df[['NA_Sales', 'EU_Sales', 'JP_Sales']].sum() / 1000
 
-regional_sales.plot(kind='bar')
+regional_sales.plot(kind='barh', color=['#9af252', '#edf252', '#e6844c'])
 
 plt.title('Vendas Totais de Jogos por Região')
 
-plt.xlabel('Região')
-plt.ylabel('Total de Vendas (bilhões)')
-plt.xticks(rotation=360)
+plt.xlabel('Total de Vendas (bilhões)')
+plt.ylabel('Região', rotation=360)
+plt.yticks(rotation=45)
+
+plt.grid(True, linestyle='--', alpha=0.7, color='gray')
 
 plt.grid(True)
 plt.show()
 
-# Total de vendas conforme os anos
-
+# VENDAS GLOBAIS AO LONGO DO TEMPO
 sales_over_time = df.groupby('Year')['Global_Sales'].sum().reset_index()
 
-# Criando a figura
 fig = go.Figure()
 
-# Adicionando o gráfico de linhas com marcadores
 fig.add_trace(go.Scatter(
     x=sales_over_time['Year'],
     y=sales_over_time['Global_Sales'],
@@ -61,7 +70,6 @@ fig.add_trace(go.Scatter(
     line=dict(width=2, color='blue')
 ))
 
-# Atualizando o layout para um design simples
 fig.update_layout(
     title='Vendas Globais ao Longo do Tempo',
     xaxis_title='Ano',
@@ -72,44 +80,45 @@ fig.update_layout(
     yaxis=dict(showgrid=True, gridcolor='lightgrey')
 )
 
-# Exibindo o gráfico
 fig.show()
 
-# Histograma de vendas por plataforma
+# HISTOGRAMA DE VENDAS POR PLATAFORMA
 
 df_grouped = df.groupby('Platform')['Global_Sales'].sum().reset_index()
 df_grouped = df_grouped.sort_values(by='Global_Sales', ascending=False)
+# df_grouped = df_grouped.sort_values(by='Platform', ascending=True)
 
 plt.figure(figsize=(24, 10))
-ax = sns.histplot(data=df_grouped, x='Platform', weights='Global_Sales', bins=10)
+ax = sns.histplot(data=df_grouped, x='Platform', weights='Global_Sales', color='#ff0008', bins=10)
 plt.title('Histograma de Vendas por Plataforma')
 plt.xlabel('Plataforma')
-plt.ylabel('Vendas (milhão)')
-plt.grid(True)
+plt.ylabel('Vendas (bilhão)')
+plt.grid(True, linestyle='--', alpha=0.7, color='gray')
 
 yticks = ax.get_yticks()
 ax.set_yticks(yticks)
-ax.set_yticklabels([f'{tick / 1000:.1f}' for tick in yticks])
 
-plt.xticks(ax.get_xticks(), rotation=90)
+ax.set_yticklabels([f'{tick / 1000:.1f}' for tick in yticks])
+plt.xticks(ax.get_xticks(), rotation=90, fontsize=12)
+
+sns.set_theme(style="whitegrid")
 
 plt.show()
 
-# Histograma de vendas por ano
-
+# HISTOGRAMA DE VENDAS POR ANO
 df_grouped = df.groupby('Year')['Global_Sales'].sum().reset_index()
 
 plt.figure(figsize=(24, 10))
 
-ax2 = sns.histplot(data=df_grouped, x='Year', weights='Global_Sales', bins=10)
+ax = sns.histplot(data=df_grouped, x='Year', weights='Global_Sales', color='#65c7a6', bins=10)
 plt.title('Histograma de Vendas por Ano de Lançamento')
 plt.xlabel('Ano de Lançamento')
-plt.ylabel('Vendas (milhão)')
-ax2.grid(True)
+plt.ylabel('Vendas (bilhão)')
+ax.grid(True)
 
-yticks = ax2.get_yticks()
-ax2.set_yticks(yticks)
-ax2.set_yticklabels([f'{tick / 1000:.1f}' for tick in yticks])
+yticks = ax.get_yticks()
+ax.set_yticks(yticks)
+
+ax.set_yticklabels([f'{tick / 1000:.1f}' for tick in yticks])
 
 plt.plot()
-plt.show()
